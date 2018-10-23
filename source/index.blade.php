7f03178b18b60734c6b371f8b86d128f372f079e
Stuart Jones <stuart@savvywombat.com.au>
2018-10-23 16:50:21 +1030

    improved styling for "more" links in blog index... replaced with "continue reading"

--- source/index.blade.php
+++ source/index.blade.php
@@ -5,8 +5,7 @@
         <div class="border-grey{{ $post == $posts->last() ? '' : ' border-b-2'}}">
             <h2 class="pt-8 pb-4"><a href="{{ $post->getPath() }}" class="no-underline text-blue-dark hover:text-blue-darker">{{ $post->title }}</a></h2>
             <p class="pb-4">{{ date("d-m-Y", $post->date) }}</p>
-            <p class="pb-4">{{ $post->excerpt() }}</p>
-            <p class="pb-4 text-right"><a href="{{ $post->getPath() }}" class="text-blue-dark hover:text-blue-darker no-underline">more...</a></p>
+            <p class="pb-4">{{ $post->excerpt() }}... <a href="{{ $post->getPath() }}" class="text-blue-dark hover:text-blue-darker no-underline font-bold">continue reading</a></p>
         </div>
     @endforeach
 @endsection
