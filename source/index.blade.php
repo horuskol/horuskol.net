62dee9b6f01bebd451beeb350fc7cd1d236b97bc
Stuart Jones <stuart@savvywombat.com.au>
2018-11-01 14:33:01 +1030

    better date format for posts list

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,7 +4,7 @@
     @foreach ($posts as $post)
         <div class="border-grey{{ $post == $posts->last() ? '' : ' border-b-2'}}">
             <h2 class="pt-8 pb-4"><a href="{{ $post->getPath() }}" class="no-underline text-blue-dark hover:text-blue-darker">{{ $post->title }}</a></h2>
-            <p class="pb-4">{{ date("d-m-Y", $post->date) }}</p>
+            <p class="pb-4">{{ date("F j, y", $post->date) }}</p>
             <p class="pb-4">{{ $post->excerpt() }}... <a href="{{ $post->getPath() }}" class="text-blue-dark hover:text-blue-darker no-underline font-bold">continue reading</a></p>
         </div>
     @endforeach
