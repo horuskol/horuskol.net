f3354363d496886d588088ec299b96682547f018
Stuart Jones <stuart@savvywombat.com.au>
2020-01-21 16:20:49 +1030

    presentations index styled more like the posts index

--- source/presentations.blade.php
+++ source/presentations.blade.php
@@ -5,7 +5,8 @@
         <div class="border-grey{{ $presentation == $presentations->last() ? '' : ' border-b-2'}}">
             <h2 class="pt-8 pb-4"><a href="{{ $presentation->getPath() }}" class="no-underline text-blue-dark hover:text-blue-darker">{{ $presentation->presentation }}</a></h2>
             <p class="pb-4">{{ date("F j, Y", $presentation->date) }}</p>
-            <p class="pb-4">{{ $presentation->summary }}... <a href="{{ $presentation->getPath() }}" class="text-blue-dark hover:text-blue-darker no-underline font-bold">start presentation</a></p>
+            <p class="pb-2">{{ $presentation->summary }}</p>
+            <p class="pb-4"><a href="{{ $presentation->getPath() }}" class="text-blue-dark hover:text-blue-darker no-underline font-bold">start presentation</a></p>
         </div>
     @endforeach
 @endsection
